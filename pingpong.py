81b926b6c5812649b2b3e3187a78b53233777fbf
Andy Stone <stonea@users.noreply.github.com>
2025-02-12 14:06:44 -0800

    updates for arg parsing
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- pingpong.py
+++ pingpong.py
@@ -4,19 +4,18 @@ import argparse, random
 parser = argparse.ArgumentParser(
   prog='SSTPingPong',
   description='Run a simulation consisting of several components arranged in a 1D or 2D grid that send messages back and forth')
-parser.add_argument('--timeToRun',  type=int, default=200)
-parser.add_argument('--numDims',    type=int, default=1)
-parser.add_argument('--N',          type=int, default=10)
-parser.add_argument('--numBalls',   type=int, default=-1)
-parser.add_argument('--edgeDelay',  type=int, default=50)
-parser.add_argument('--random',     default=False, action='store_true')
-parser.add_argument('--verbose',    default=False, action='store_true')
+parser.add_argument('--N',              type=int, default=10)
+parser.add_argument('--timeToRun',      type=int, default=200)
+parser.add_argument('--numDims',        type=int, choices=[1,2], default=2)
+parser.add_argument('--edgeDelay',      type=int, default=50)
+parser.add_argument('--artificialWork', type=int, default=0)
+parser.add_argument('--verbose',        default=False, action='store_true')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('--corners',         default=False, action='store_true')
+group.add_argument('--random',          type=int, default=-1)
+group.add_argument('--randomOverlap',   type=int, default=-1)
+group.add_argument('--wavefront',       default=False, action='store_true')
 args = parser.parse_args()
-
-if args.numBalls == -1 and args.numDims == 1:
-  args.numBalls = 2
-elif args.numBalls == -1 and args.numDims == 2:
-  args.numBalls = 8
 
 # -----------------------------------------------------------------------------
 
@@ -29,13 +28,15 @@ def link(x, y, direction):
   global numLinks
   if args.verbose:
     print("connect ", x.getFullName(), direction, "--", y.getFullName(), oppositeDir(direction))
-  sst.Link("link%i" % numLinks).connect( (x, "%sPort" % direction, "%ips" % args.edgeDelay), (y, "%sPort" % oppositeDir(direction), "%ips" % args.edgeDelay) )
+  sst.Link("link%i" % numLinks).connect( (x, "%sPort" % direction, "%is" % args.edgeDelay), (y, "%sPort" % oppositeDir(direction), "%is" % args.edgeDelay) )
   numLinks += 1
 
 # -----------------------------------------------------------------------------
 
 ballGen = sst.Component("sim", "pingpong.simulator")
-ballGen.addParams({"timeToRun" : args.timeToRun})
+ballGen.addParams({"timeToRun"      : args.timeToRun,
+                   "verbose"        : args.verbose,
+                   "artificialWork" : args.artificialWork})
 
 pingPongers = []
 ballsHeadingNorthAt = {}
@@ -48,33 +49,41 @@ NE_PONGER = args.N-1
 SW_PONGER = args.N * (args.N-1)
 SE_PONGER = (args.N * args.N) - 1
 
-# Assign balls in corners if we're not setting them randomly
-if not args.random:
-  if args.numBalls >= 1:
-    ballsHeadingEastAt[NW_PONGER] = 1
-  if args.numBalls >= 2:
-    ballsHeadingWestAt[NE_PONGER] = 1
+if args.corners:
+  ballsHeadingEastAt[NW_PONGER] = 1
+  ballsHeadingWestAt[NE_PONGER] = 1
   if args.numDims > 1:
-    if args.numBalls >= 3:
       ballsHeadingEastAt[SW_PONGER] = 1
-    if args.numBalls >= 4:
       ballsHeadingWestAt[SE_PONGER] = 1
-    if args.numBalls >= 5:
       ballsHeadingSouthAt[NW_PONGER] = 1
-    if args.numBalls >= 6:
       ballsHeadingSouthAt[NE_PONGER] = 1
-    if args.numBalls >= 7:
       ballsHeadingNorthAt[SW_PONGER] = 1
-    if args.numBalls >= 8:
       ballsHeadingNorthAt[SE_PONGER] = 1
-else:
-  sample = random.sample(range(args.N * args.N), args.numBalls)
+elif args.random != -1:
+  sample = random.sample(range(args.N * args.N), args.random)
   for r in sample:
     direction = random.randint(0,3)
     if direction   == 0: ballsHeadingNorthAt[r] = 1
     elif direction == 1: ballsHeadingSouthAt[r] = 1
     elif direction == 2: ballsHeadingWestAt[r]  = 1
     elif direction == 3: ballsHeadingEastAt[r]  = 1
+elif args.randomOverlap != -1:
+  for _ in range(args.randomOverlap):
+    r = random.randint(0, args.N * args.N - 1)
+    direction = random.randint(0,3)
+    if direction   == 0: ballsHeadingNorthAt[r] = 1
+    elif direction == 1: ballsHeadingSouthAt[r] = 1
+    elif direction == 2: ballsHeadingWestAt[r]  = 1
+    elif direction == 3: ballsHeadingEastAt[r]  = 1
+elif args.wavefront:
+  for i in range(0,args.N):
+     ballsHeadingSouthAt[i]                   = 1
+     ballsHeadingEastAt[i*args.N]             = 1
+     ballsHeadingWestAt[(i+1)*args.N-1]       = 1
+     ballsHeadingNorthAt[args.N*(args.N-1)+i] = 1
+else:
+  print("Unknown ball placement configuration")
+  exit(1)
 
 if args.verbose:
   print("Initial balls --")
