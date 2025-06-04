55e29c1827870fd6d50892c7b288fa1670e404da
Andy Stone <stonea@users.noreply.github.com>
2025-06-04 10:05:42 -0700

    adds comments
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- gameoflife/gol.py
+++ gameoflife/gol.py
@@ -14,8 +14,15 @@ sst.setProgramOption("stop-at", args.stop_at)
 myRank = sst.getMyMPIRank()
 numRanks = sst.getMPIRankCount()
 
-# ranks 0-(args.N%numRanks) will have args.N / numRanks + 1 rows
-# and all subsequent ranks will have args.N / numRanks
+# We do a block-row partitioning of the game board.  Because the number of rows
+# might not be divisible my the number of MPI ranks we will have ranks 0
+# throuhg (args.N%numRanks) have (args.N / numRanks + 1) rows # and all
+# subsequent ranks will have (args.N / numRanks).  IOW we evenly divide as best
+# we can, and then add an extra row to the first (args.N%numRanks) to get us to
+# our desired game board size.
+#
+# myRowStart and myRowEnd produce na inclusive range (IOW this rank owns row
+# 'myRowEnd'). 
 myRowStart = int(args.N / numRanks) * myRank + min(myRank, args.N % numRanks)
 myRowEnd   = myRowStart + int(args.N / numRanks) - 1
 if myRank < args.N % numRanks:
@@ -28,30 +35,41 @@ links = set()
 def createLink(srcRow, srcCol, offRow, offCol, srcPort, tgtPort):
   tgtRow = srcRow + offRow
   tgtCol = srcCol + offCol
+
+  # Don't create a link that would go off the board
   if tgtRow < 0 or tgtCol < 0 or tgtRow >= args.N or tgtCol >= args.N:
     return
+
+  # Don't create a link to a component that has not been made
   if (srcRow not in cells) or (tgtRow not in cells):
     return
 
   srcComp = cells[srcRow][srcCol]
   tgtComp = cells[tgtRow][tgtCol]
 
+  # We want to 'normalize' link names so that a link that goes from component
+  # 'x' to 'y' and a link that goes from component 'y' to 'x' will have the
+  # same name.
   minRow = min( (srcRow,srcCol), (tgtRow,tgtCol) )[0]
   minCol = min( (srcRow,srcCol), (tgtRow,tgtCol) )[1]
   maxRow = max( (srcRow,srcCol), (tgtRow,tgtCol) )[0]
   maxCol = max( (srcRow,srcCol), (tgtRow,tgtCol) )[1]
   name = "link_%i_%i__%i_%i" % (minRow,minCol,maxRow,maxCol)
 
+  # Avoid making duplicate links
   if not name in links:
     sst.Link(name).connect( (srcComp, srcPort, "1s"), (tgtComp, tgtPort, "1s") )
     links.add(name)
 
 # -----------------------------------------------------------------------------
+
+# First create the portion of the game board this rank is responsible for as
+# well as one row below and above what this rank is responsible for (so that we
+# can connect to it).
 r = random.randint(0, args.N * args.N - 1)
 for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
   cells[row] = {}
   for col in range(0, args.N):
-    rval = random.randint(0,100)
     cell = sst.Component("cell_%i_%i" % (row,col), "gol.cell")
     cells[row][col] = cell
     if row < myRowStart:
@@ -59,11 +77,12 @@ for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
     elif row > myRowEnd:
       cell.setRank(myRank+1)
     else:
-      cell.addParams({"isAlive": rval <= args.prob})
       cell.setRank(myRank)
+      rval = random.randint(0,100)
+      cell.addParams({"isAlive": rval <= args.prob})
 
-
-for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
+# Create links for all components owned by this rank
+for row in range(max(0,myRowStart), min(args.N,myRowEnd+1)):
   for col in range(0, args.N):
     createLink(row, col, -1, -1, "nwPort", "sePort" )
     createLink(row, col, -1,  0, "nPort",  "sPort"  )
@@ -73,18 +92,3 @@ for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
     createLink(row, col,  1, -1, "swPort", "nePort" )
     createLink(row, col,  1,  0, "sPort",  "nPort"  )
     createLink(row, col,  1,  1, "sePort", "nwPort" )
-
-
-
-#r = random.randint(0, args.N * args.N - 1)
-#for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
-#  cells[row] = {}
-#  for col in range(0, args.N):
-#    rval = random.randint(0,100)
-#    cell = sst.Component("cell_%i_%i" % (row,col), "gol.cell")
-#    cells[row][col] = cell
-#
-#    createLink(row, col,  0, -1, "wPort",  "ePort" )
-#    createLink(row, col, -1, -1, "nwPort", "swPort")
-#    createLink(row, col, -1,  0, "nPort",  "sPort" )
-#    createLink(row, col, -1,  1, "nePort", "sePort")
