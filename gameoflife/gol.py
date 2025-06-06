2e793d115fa104d8e984e922caa698bf3433ca82
Andy Stone <stonea@users.noreply.github.com>
2025-06-06 13:52:23 -0500

    adds arg for rectangular board
    ---
    Signed-off-by: Andy Stone <stonea@users.noreply.github.com>

--- gameoflife/gol.py
+++ gameoflife/gol.py
@@ -5,6 +5,7 @@ parser = argparse.ArgumentParser(
   prog='GameOfLife',
   description="Conway's Game-Of-Life")
 parser.add_argument('--N',       type=int, default=10)
+parser.add_argument('--M',       type=int, default=-1)
 parser.add_argument('--prob',    type=int, default=30)
 parser.add_argument('--stop-at', default="5s")
 args = parser.parse_args()
@@ -14,18 +15,21 @@ sst.setProgramOption("stop-at", args.stop_at)
 myRank = sst.getMyMPIRank()
 numRanks = sst.getMPIRankCount()
 
+if args.M == -1:
+  args.M = args.N
+
 # We do a block-row partitioning of the game board.  Because the number of rows
 # might not be divisible my the number of MPI ranks we will have ranks 0
-# throuhg (args.N%numRanks) have (args.N / numRanks + 1) rows # and all
-# subsequent ranks will have (args.N / numRanks).  IOW we evenly divide as best
-# we can, and then add an extra row to the first (args.N%numRanks) to get us to
+# throuhg (args.M%numRanks) have (args.M / numRanks + 1) rows # and all
+# subsequent ranks will have (args.M / numRanks).  IOW we evenly divide as best
+# we can, and then add an extra row to the first (args.M%numRanks) to get us to
 # our desired game board size.
 #
 # myRowStart and myRowEnd produce na inclusive range (IOW this rank owns row
 # 'myRowEnd'). 
-myRowStart = int(args.N / numRanks) * myRank + min(myRank, args.N % numRanks)
-myRowEnd   = myRowStart + int(args.N / numRanks) - 1
-if myRank < args.N % numRanks:
+myRowStart = int(args.M / numRanks) * myRank + min(myRank, args.M % numRanks)
+myRowEnd   = myRowStart + int(args.M / numRanks) - 1
+if myRank < args.M % numRanks:
   myRowEnd += 1
 
 # -----------------------------------------------------------------------------
@@ -37,7 +41,7 @@ def createLink(srcRow, srcCol, offRow, offCol, srcPort, tgtPort):
   tgtCol = srcCol + offCol
 
   # Don't create a link that would go off the board
-  if tgtRow < 0 or tgtCol < 0 or tgtRow >= args.N or tgtCol >= args.N:
+  if tgtRow < 0 or tgtCol < 0 or tgtRow >= args.M or tgtCol >= args.N:
     return
 
   # Don't create a link to a component that has not been made
@@ -66,8 +70,7 @@ def createLink(srcRow, srcCol, offRow, offCol, srcPort, tgtPort):
 # First create the portion of the game board this rank is responsible for as
 # well as one row below and above what this rank is responsible for (so that we
 # can connect to it).
-r = random.randint(0, args.N * args.N - 1)
-for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
+for row in range(max(0,myRowStart-1), min(args.M,myRowEnd+2)):
   cells[row] = {}
   for col in range(0, args.N):
     cell = sst.Component("cell_%i_%i" % (row,col), "gol.cell")
@@ -82,7 +85,7 @@ for row in range(max(0,myRowStart-1), min(args.N,myRowEnd+2)):
       cell.addParams({"isAlive": rval <= args.prob})
 
 # Create links for all components owned by this rank
-for row in range(max(0,myRowStart), min(args.N,myRowEnd+1)):
+for row in range(max(0,myRowStart), min(args.M,myRowEnd+1)):
   for col in range(0, args.N):
     createLink(row, col, -1, -1, "nwPort", "sePort" )
     createLink(row, col, -1,  0, "nPort",  "sPort"  )
