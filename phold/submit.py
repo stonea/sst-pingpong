597abde453898e9b28da08e49ebdbc3cdfae7f91
Brandon Neth <brandon-neth@github.hpe.com>
2025-07-20 11:43:40 -0500

    added support for stochastic, component-per-node runs
    Signed-off-by: Brandon Neth <brandon-neth@github.hpe.com>

--- phold/submit.py
+++ phold/submit.py
@@ -43,7 +43,8 @@ def parse_arguments():
   # In such a circumstance, we would want to have the per-node component count be used to dynamically calculate the necessary widths.
   # Or maybe, we want to say "the first width is the value that calculates the per-node component count. Vary widths as the height changes to maintain the per-node component count."
 
-  parser.add_argument('--weak_scaling', '--weak', '--weak-scaling', action='store_true', help="If set, use weak scaling. The height will be multiplied by the node count to maintain the per-node linking.")
+  parser.add_argument('--weak_scaling', '--weak', '--weak-scaling', action='store_true', help="If set, the height parameters are treated as \
+                      per-node heights rather. If not, then the height parameters are treated as the total grid height.")
   parser.add_argument('--stochastic', type=int, help="If set, treat the arguments to this script as integer constants or range bounds, \
                       rather than lists of values. The value of this variable is the number of points in the resulting space to sample.")
   args = parser.parse_args()
@@ -51,15 +52,84 @@ def parse_arguments():
   return args
 
 
+
+
+def convert_to_ranges(args):
+  if args.stochastic is not None:
+    # Turn the single values into ranges with a single element
+    if len(args.node_counts) == 1:
+      args.node_counts = [args.node_counts[0]] * 2
+    if args.widths is not None and len(args.widths) == 1:
+      args.widths = [args.widths[0]] * 2
+    if len(args.heights) == 1:
+      args.heights = [args.heights[0]] * 2
+    if len(args.event_densities) == 1:
+      args.event_densities = [args.event_densities[0]] * 2
+    if len(args.ring_sizes) == 1:
+      args.ring_sizes = [args.ring_sizes[0]] * 2
+    if len(args.times_to_run) == 1:
+      args.times_to_run = [args.times_to_run[0]] * 2
+    if len(args.small_payloads) == 1:
+      args.small_payloads = [args.small_payloads[0]] * 2
+    if len(args.large_payloads) == 1:
+      args.large_payloads = [args.large_payloads[0]] * 2
+    if len(args.large_event_fractions) == 1:
+      args.large_event_fractions = [args.large_event_fractions[0]] * 2
+    if args.components_per_node is not None and len(args.components_per_node) == 1:
+      args.components_per_node = [args.components_per_node[0]] * 2
+  return args
+
+
+# 
+def stochastic_grid_shapes(args):
+  '''
+  Calculate grid shapes for stochastic runs. 
+  We are treating the arguments as ranges, and need to sample from them based 
+  on if we are doing weak scaling and if the components_per_node or width is specified
+  '''
+
+  node_counts = []
+  for i in range(args.stochastic):
+    node_count = random.randint(args.node_counts[0], args.node_counts[1])
+    node_counts.append(node_count)
+
+  grid_heights = []
+  for i in range(args.stochastic):
+    height = random.randint(args.heights[0], args.heights[1])
+    if args.weak_scaling:
+      grid_heights.append(height)
+    else:
+      grid_heights.append(height * node_counts[i])
+  
+  grid_widths = []
+  if args.components_per_node is not None:
+    for i in range(args.stochastic):
+      per_node_component_count = random.randint(args.components_per_node[0], args.components_per_node[1])
+      component_count = node_counts[i] * per_node_component_count
+      grid_width = math.ceil(component_count / grid_heights[i])
+      grid_widths.append(grid_width)
+  else:
+    for i in range(args.stochastic):
+      grid_widths.append(random.randint(args.widths[0], args.widths[1]))
+    
+  return list(zip(grid_widths, grid_heights, node_counts))
+
+
+# The weak scaling determines if the "height" parameter is a per-node value or the entire grid.
 def calculate_grid_shapes(args):
+  '''
+  Creates a list of tuples representing the global grid shape and node counts for the different runs.
+  Each tuple is (width, height, node_count)
+  '''
   shapes = []
   if not args.weak_scaling and args.components_per_node is None:
     return list(itertools.product(args.widths, args.heights, args.node_counts))
   elif args.components_per_node is None:
+    # Weak scaling on height per node
     for per_node_width, per_node_height, node_count in itertools.product(args.widths, args.heights, args.node_counts):
       shapes.append((per_node_width, per_node_height * node_count, node_count))
-  
   elif not args.weak_scaling:
+    # Weak scaling
     for per_node_component_count, grid_height, node_count in itertools.product(args.components_per_node, args.heights, args.node_counts):
       component_count = node_count * per_node_component_count
       grid_width = math.ceil(component_count / grid_height)
@@ -81,51 +151,34 @@ if __name__ == "__main__":
   subprocess.run("make", shell=True, check=True)
   os.chdir(working_dir)
 
-  if args.stochastic:
+  args = convert_to_ranges(args)
+  if args.stochastic is not None:
     
-    # Turn the single values into ranges with a single element
-    if len(args.node_counts) == 1:
-      args.node_counts = [args.node_counts[0]] * 2
-    if len(args.widths) == 1:
-      args.widths = [args.widths[0]] * 2
-    if len(args.heights) == 1:
-      args.heights = [args.heights[0]] * 2
-    if len(args.event_densities) == 1:
-      args.event_densities = [args.event_densities[0]] * 2
-    if len(args.ring_sizes) == 1:
-      args.ring_sizes = [args.ring_sizes[0]] * 2
-    if len(args.times_to_run) == 1:
-      args.times_to_run = [args.times_to_run[0]] * 2
-    if len(args.small_payloads) == 1:
-      args.small_payloads = [args.small_payloads[0]] * 2
-    if len(args.large_payloads) == 1:
-      args.large_payloads = [args.large_payloads[0]] * 2
-    if len(args.large_event_fractions) == 1:
-      args.large_event_fractions = [args.large_event_fractions[0]] * 2
+    shape_parameters = stochastic_grid_shapes(args)
     
-    shape_parameters = []
     non_shape_parameters = []
     for i in range(args.stochastic):
       density = round(random.uniform(*args.event_densities), 2)
       non_shape_point = (density, random.randint(*args.ring_sizes), random.randint(*args.times_to_run), random.randint(*args.small_payloads),
               random.randint(*args.large_payloads), random.uniform(*args.large_event_fractions))
-      shape_point = (random.randint(*args.node_counts), random.randint(*args.widths), random.randint(*args.heights))
-      shape_parameters.append(shape_point)
       non_shape_parameters.append(non_shape_point)
 
+    parameters = list(zip(shape_parameters, non_shape_parameters))
   else:
+    # Non stochastic run, so we do cartesian product of the parameters
+    
     shape_parameters = calculate_grid_shapes(args)
     non_shape_parameters = list(itertools.product(args.event_densities, args.ring_sizes, args.times_to_run,
                                         args.small_payloads, args.large_payloads, args.large_event_fractions))
+    parameters = list(itertools.product(shape_parameters, non_shape_parameters))
 
-  print("Shapes: ", shape_parameters)
-  for (width, height, node_count) in shape_parameters:
-    for (event_density, ring_size, time_to_run, small_payload, large_payload, large_event_fraction) in non_shape_parameters:
-      output_file = f"{args.name}_{node_count}_{width}_{height}_{event_density}_{ring_size}_{time_to_run}_{small_payload}_{large_payload}_{large_event_fraction}"
-      sbatch_portion = f"sbatch -N {node_count} -o {output_file}.out"
-      command = f"{sbatch_portion} {script_dir}/dispatch.sh {node_count} {width} {height} {event_density} {ring_size} {time_to_run} {small_payload} {large_payload} {large_event_fraction} {output_file}"
-      print(command)
-      if not args.dry_run:
-        print(f"Running: {command}")
-        subprocess.run(command, shell=True, check=True)
+  print("parameters: ", parameters)
+  for ((width, height, node_count), (event_density, ring_size, time_to_run, small_payload, large_payload, large_event_fraction)) in parameters:
+    output_file = f"{args.name}_{node_count}_{width}_{height}_{event_density}_{ring_size}_{time_to_run}_{small_payload}_{large_payload}_{large_event_fraction}"
+    sbatch_portion = f"sbatch -N {node_count} -o {output_file}.out"
+    command = f"{sbatch_portion} {script_dir}/dispatch.sh {node_count} {width} {height} {event_density} {ring_size} {time_to_run} {small_payload} {large_payload} {large_event_fraction} {output_file}"
+    print(command)
+    if not args.dry_run:
+      print(f"Running: {command}")
+      subprocess.run(command, shell=True, check=True)
       
